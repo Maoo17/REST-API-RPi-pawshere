12a2f5935ec2758e26d76f7c644ce402f9560ccd
Matilda Olsson <31142769+Maoo17@users.noreply.github.com>
2018-10-03 22:10:42 +0200

    Delete setup_server.sql

--- sql/setup_server.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS paws;
-CREATE DATABASE paws CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE paws;
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' IDENTIFIED BY 'muddskippedipp';
