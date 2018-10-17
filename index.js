b6cc7544ca5ef441818f5cbfe862507f407d9c95
Matilda <matildaytc@gmail.com>
2018-10-17 15:51:08 +0100

    6th commit

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(middleware.logIncomingToConsole);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routeIndex);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
 app.listen(port, logStartUpDetailsToConsole);
 
 
