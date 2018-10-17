f486ef979edb75043e3566019d0f538abed31e7e
Matilda <matildaytc@gmail.com>
2018-10-17 16:20:17 +0100

    7th commit

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ const routeIndex = require("./route/index.js");
 const middleware = require("./middleware/index.js");
 const bodyParser = require('body-parser');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(middleware.logIncomingToConsole);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routeIndex);
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 app.listen(port, logStartUpDetailsToConsole);
 
 
