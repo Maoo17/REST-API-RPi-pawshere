b4ea9ad280a025dae68abff2fd7078a07b6420d3
Matilda <matildaytc@gmail.com>
2018-10-20 12:43:16 +0100

    hotfix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const middleware = require("./middleware/index.js");
 const bodyParser = require('body-parser');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost, *");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
