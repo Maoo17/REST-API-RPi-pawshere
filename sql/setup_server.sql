c24820a7d494eed0819c1b2be6feb94fd2283320
Matilda Olsson <31142769+Maoo17@users.noreply.github.com>
2018-11-01 14:16:21 +0100

    Update setup_server.sql

--- sql/setup_server.sql
+++ sql/setup_server.sql
@@ -1,4 +1,4 @@
 DROP DATABASE IF EXISTS paws;
 CREATE DATABASE paws CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE paws;
-GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' IDENTIFIED BY 'muddskippedipp';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost' IDENTIFIED BY 'secret';
